1856dfc311903de81b26024e8869b5af6fee1d89
Brendon Mendicino <brendonmedicino@yahoo.it>
2023-03-19 16:18:10 +0100

    Added screenshot listener.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use core::time;
 use std::cmp::max;
 use std::collections::HashSet;
 use std::sync::{Arc, Mutex};
-use std::io;
+use std::io::{self, stdout, Write};
 use std::path::PathBuf;
+use std::thread;
 
 use clap::Parser;
 
@@ -47,17 +49,23 @@ fn print_menu(paths: &HashSet<PathBuf>) {
     );
     let ceiling = (0..max_len).map(|_| "#").collect::<String>();
 
+
     println!("{}", ceiling);
     for (i, p) in paths.iter().enumerate() {
         let item = format!("# {:3}. {}", i, p.display());
         let end = (0..(max_len - item.chars().count() - 1)).map(|_| " ").collect::<String>() + "#";
         println!("{}{}", item, end);
-        println!("#{}#", (0..(max_len - 2)).map(|_| " ").collect::<String>())
+
+        let spaces = (0..(max_len - 2)).map(|_| " ").collect::<String>();
+        println!("#{}#", spaces);
     }
     println!("{}", ceiling);
 }
 
 fn choose_working_dir(paths: &HashSet<PathBuf>) -> Result<PathBuf, io::Error> {
+
+    print!("> ");
+    stdout().lock().flush()?;
 
     // Get input position
     let mut input = String::new();
@@ -75,12 +83,38 @@ fn choose_working_dir(paths: &HashSet<PathBuf>) -> Result<PathBuf, io::Error> {
             false => None
         });
 
+
     match res {
         Some(val) => Ok(val.to_path_buf()),
         None => Err(io::Error::new(io::ErrorKind::Other, String::from("Path does not exist!"))),
     }
 }
 
+
+fn screeshot_listener(image_path: &PathBuf, state: Arc<Mutex<State>>) {
+    let mut old_images = get_images(image_path);
+
+    loop {
+        thread::sleep(time::Duration::from_secs(3));
+
+        let state = state.lock().unwrap();
+
+        match *state { 
+            State::Idle => continue,
+            State::Stopped => break,
+            _ => println!("FRATM"),
+        }
+
+        let new_images = get_new_images(image_path, &old_images);
+
+        for image in &new_images {
+
+        }
+
+        // refresh images
+        old_images = new_images;
+    }
+}
 
 fn main() {
     let args = ScreenshotArgs::parse();
@@ -90,19 +124,32 @@ fn main() {
     
     let notes = get_note_dirs(&args.note);
 
+
+
+    let state_screen = Arc::clone(&state);
+    let screenshot_handle = thread::spawn(move || screeshot_listener(&args.screenshot, state_screen));
+
     loop {
+        thread::sleep(time::Duration::from_secs(1));
+
         let mut state = state.lock().unwrap();
         println!("{:?}", *state);
 
+        if let State::Stopped = *state { break; }
 
         print_menu(&notes);
-        let result = choose_working_dir(&notes);
-        if let Err(err) = result {
-            eprintln!("An error accurred: {}", err.to_string());
-            continue;
-        }
-        
-        *state = State::Listening(result.unwrap());
+        let result = match choose_working_dir(&notes) {
+            Ok(val) => val,
+            Err(err) => {
+                eprintln!("An error accurred: {}", err.to_string());
+                continue;
+            }
+        };
+                
+        *state = State::Listening(result);
     }
+
+
+    screenshot_handle.join().unwrap();
 }
 
